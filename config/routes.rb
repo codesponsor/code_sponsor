49e25fb80d9294dc5d175f56a53ffecd230b954f
Eric Berry <cavneb@gmail.com>
2018-02-17 22:42:24 -0700

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
-    root to: "users#index"
-  end
+  # devise_for :admin_users, { class_name: "User" }.merge(ActiveAdmin::Devise.config)
+  # ActiveAdmin.routes(self)
 
   devise_for :users
+
+  authenticated :user do
+    root to: "react_app#index"
+  end
+
+  root to: "home#index"
 end
